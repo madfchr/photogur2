cbbcc3c5b4df4484413e1ba6ef01fcadb78a9394
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-09 11:27:50 -0400

    Create Sunset model in database

---
+++ db/migrate/20160409152059_create_sunsets.rb
@@ -0,0 +1,11 @@
+class CreateSunsets < ActiveRecord::Migration
+  def change
+    create_table :sunsets do |t|
+      t.string :title
+      t.string :city
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
