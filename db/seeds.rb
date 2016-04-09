beb7a6b3e3994197cf2204b1434c8345ca3c6777
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-09 11:44:03 -0400

    Creating some default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
+Sunset.create!(
+:title => "Down the Alley Downtown LA"
+:city => "Los Angeles"
+:url => "http://40.media.tumblr.com/b9417f19addf5c7f39e06d863a8eb6a4/tumblr_o0j0dx5K5Z1uv9ox1o2_500.jpg"
+)
+
+Sunset.create!(
+:title => "Sunnyside Bliss"
+:city => "Sunnyside, Queens"
+:url => "http://41.media.tumblr.com/359b8e0d5283b2b24d323b2acdf7956b/tumblr_o0j08aqcd51uv9ox1o1_1280.jpg"
+)
+
+Sunset.create!(
+:title => "This is actually a sunrise. 6:30 AM"
+:city => "Bushwick, Brooklyn"
+:url => "http://36.media.tumblr.com/dff6ec2936bf2fb54a1d0138683132d1/tumblr_nv8n52LUG81uv9ox1o1_1280.jpg"
+)
+
+Sunset.create!(
+:title => "Little Beachiness"
+:city => "Kihei, Maui"
+:url => "http://40.media.tumblr.com/fe73a62d741b80d7362b138fddc11321/tumblr_no3dm7nAh41uv9ox1o1_1280.jpg"
+)
+
+Sunset.create!(
+:title => "Yummy Landing"
+:city => "St-John's, Newfoundland"
+:url => "http://40.media.tumblr.com/76b03f6ba781ca2bf7a92b71387e55fe/tumblr_no3a1o66PC1uv9ox1o1_1280.jpg"
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
